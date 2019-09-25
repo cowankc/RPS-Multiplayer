7caf17447a895203166438202a596efe24039996
cowankc <cowankc92@live.com>
2019-09-25 10:41:41 -0500

    added a chat box for messaaging and allowed players to enter their name, reworked some of the database variables

--- docs/assets/javascript/logic.js
+++ docs/assets/javascript/logic.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+    //variables//
+    let player1 = null;
+    let player2 = null;
+    let namePlayer1 = "";
+    let namePlayer2 = "";
+    let p1Choice = "";
+    let p2Choice = "";
+
+
     let firebaseConfig = {
         apiKey: "AIzaSyDOTTdhYcgCi6UBUZ5a1gF4TjhX23-BWko",
         authDomain: "rps-game-8f3bc.firebaseapp.com",
@@ -15,43 +24,56 @@ $(document).ready(function(){
 
     let database = firebase.database()
     
-
     database.ref().on("value", function(snapshot) {
-        let p1WinCount = 0;
-        let p1LoseCount = 0;
-        let p2WinCount = 0;
-        let p2LoseCount = 0;
-        let ties = 0
-        let user1
-        let user2
-        $("#p1Wins").text(p1WinCount);
-        $("#p1Loses").text(p1LoseCount);
-        $("#player1Ties").text(ties);
-        $("#p2Wins").text(p2WinCount);
-        $("#p2Loses").text(p2LoseCount);
-        $("#player2Ties").text(ties);
+        player1 = snapshot.val().player1;
+        namePlayer1 = player1.name
+        $("#p1Name").text(namePlayer1)
+
+        player2 = snapshot.val().player2;
+        namePlayer2 = player2.name
+        $("#p2Name").text(namePlayer2)
+
+        if (player1 && player2) {
+            
+        }
+
+
+        // let p1WinCount = 0;
+        // let p1LoseCount = 0;
+        // let p2WinCount = 0;
+        // let p2LoseCount = 0;
+        // let ties = 0
+        
+        // $("#p1Wins").text(p1WinCount);
+        // $("#p1Loses").text(p1LoseCount);
+        // $("#player1Ties").text(ties);
+        // $("#p2Wins").text(p2WinCount);
+        // $("#p2Loses").text(p2LoseCount);
+        // $("#player2Ties").text(ties);
+
+        // $("#p1").on("click", function() {
+        //     let rockBtn = "<button id='rock1'>Rock</button>"
+        //     let paperBtn = "<button id='paper1'>Paper</button>"
+        //     let scissorBtn = "<button id='scissor1'>Scissor</button>"
+        //     $("#p1Info").append(rockBtn)
+        //     $("#p1Info").append(paperBtn)
+        //     $("#p1Info").append(scissorBtn)
+        //     $("#p1").remove()
+        //     player1 = user1
+        // })
+        // $("#p2").on("click", function() {
+        //     let rockBtn = "<button id='rock2'>Rock</button>"
+        //     let paperBtn = "<button id='paper2'>Paper</button>"
+        //     let scissorBtn = "<button id='scissor2'>Scissor</button>"
+        //     $("#p2Info").append(rockBtn)
+        //     $("#p2Info").append(paperBtn)
+        //     $("#p2Info").append(scissorBtn)
+        //     $("#p2").remove()
+        //     player2 = user2 
+        // })
+        
+        
     });
-    $("#p1").on("click", function() {
-        let rockBtn = "<button id='rock1'>Rock</button>"
-        let paperBtn = "<button id='paper1'>Paper</button>"
-        let scissorBtn = "<button id='scissor1'>Scissor</button>"
-        $("#p1Info").append(rockBtn)
-        $("#p1Info").append(paperBtn)
-        $("#p1Info").append(scissorBtn)
-        $("#p1").remove()
-    })
-    $("#p2").on("click", function() {
-        let rockBtn = "<button id='rock2'>Rock</button>"
-        let paperBtn = "<button id='paper2'>Paper</button>"
-        let scissorBtn = "<button id='scissor2'>Scissor</button>"
-        $("#p2Info").append(rockBtn)
-        $("#p2Info").append(paperBtn)
-        $("#p2Info").append(scissorBtn)
-        $("#p2").remove()
-    })
-
-
-
 
 });
 
